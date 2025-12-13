4a790c0d6bc71b34907aefcc70bfb0ba95cc6e6f
MahmudElyamani <mahmoud.elyamani.7@gmail.com>
2025-12-13 18:17:50 +0200

    Create Assignment1ByMe.c
    Solution to Assignment 1 from Unit 2 Lesson 3 in embedded diploma.
    solved by Mahmoud Elyamani

---
+++ Assignment1ByMe.c
@@ -0,0 +1,65 @@
+//Assignment 1
+//Unit 2 Lesson 3
+//by: Mahmoud Elyamani
+
+#include <stdio.h>
+int main()
+{
+    /* Ex.1:
+    printf("C Programming");
+    */
+
+    /*Ex.2:
+    int i;
+    printf("Enter an integer: ");
+    scanf("%d",&i);
+    printf("You've entered: %d",i);
+    */
+
+    /*Ex.3:
+    int i1,i2;
+    printf("Enter two integers: ");
+    scanf("%d %d",&i1, &i2);
+    printf("Sum: %d",i1+i2);
+    */
+
+    /*Ex.4:
+    float n1,n2;
+    printf("Enter two numbers: ");
+    scanf("%f %f",&n1, &n2);
+    printf("Product: %1.6f",n1*n2);
+    */
+
+    /*Ex.5:
+    char a;
+    printf("Enter a character: ");
+    scanf("%c",&a);
+    printf("ASCII value of %c = %d",a,a);
+    */
+
+    /*Ex.6:
+    float a,b,c;
+    printf("Enter value of a: ");
+    scanf("%f",&a);
+    printf("Enter value of b: ");
+    scanf("%f",&b);
+    c = b;
+    b = a;
+    a = c;
+    printf("After swapping, value of a = %2.2f\n\r",a);
+    printf("After swapping, value of b = %2.2f",b);
+    */
+
+    /*Ex.7:
+    float a,b;
+    printf("Enter value of a: ");
+    scanf("%f",&a);
+    printf("Enter value of b: ");
+    scanf("%f",&b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping, value of a = %2.2f\n\r",a);
+    printf("After swapping, value of b = %2.2f",b);
+    */
+}
