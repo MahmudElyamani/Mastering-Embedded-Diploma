1747b595fe1c7df9115a654b67874e619c720a7b
MahmudElyamani <mahmoud.elyamani.7@gmail.com>
2026-02-03 06:44:40 +0200

    Implement array input and reverse display using pointers

---
+++ Assignment6_Pointers/Question4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[15], aSize, *p, i;
+    p = arr;
+    printf("Enter the number of elements to store in the array (max 15): ");
+    scanf("%d",&aSize);
+    printf("Input the %d numbers of the array: \n",aSize);
+    for(i=0; i<aSize; i++)
+    {
+        printf("element-%d: ",i+1);
+        scanf("%d",p);
+        p++;
+    }
+    printf("The elements of the array in reverse order are: \n");
+    for(i=0; i<aSize; i++)
+    {
+        p--;
+        printf("element-%d : %d\n",i,*p);
+    }
+}
