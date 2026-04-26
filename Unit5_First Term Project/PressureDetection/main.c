b711751f6442b76304ea78b0d9771b5b140cbaee
MahmudElyamani <mahmoud.elyamani.7@gmail.com>
2026-04-26 09:51:50 +0200

    idk

---
+++ Unit5_First Term Project/PressureDetection/main.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "driver/driver.h"
+#include "alarm.h"
+#include "driver.h"
+
+int main (){
+	GPIO_INITIALIZATION();
+	while (1)
+	{
+		//Implement your Design 
+	}
+
+}
