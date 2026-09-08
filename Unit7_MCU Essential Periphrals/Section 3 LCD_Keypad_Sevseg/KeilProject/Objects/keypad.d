1712c0fd999f148e7517f09a8494d11941a623dc
MahmudElyamani <mahmoud.elyamani.7@gmail.com>
2026-09-08 10:53:07 +0200

    Switched STM32 CubeIDE with Keil uVision for code

---
+++ Unit7_MCU Essential Periphrals/Section 3 LCD_Keypad_Sevseg/KeilProject/Objects/keypad.d
@@ -0,0 +1,4 @@
+./objects/keypad.o: HAL\KEYPAD_DRIVER\keypad.c HAL\KEYPAD_DRIVER\keypad.h \
+  MCAL\stm32f103x6.h D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  MCAL\GPIO_Drivers\Stm32_F103C6_gpio_driver.h
